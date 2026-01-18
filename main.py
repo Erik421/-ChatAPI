82f706234b71b23dd363ccfe95ecd79390ef1f00
Erik421 <erikmirzoyan22@gmail.com>
2026-01-19 02:33:49 +0300

    1

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from routers import chats
 from database import Base, engine
 import models
 
-Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(title="Chats API")
 
 app.include_router(chats.router)
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
